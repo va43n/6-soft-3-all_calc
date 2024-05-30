49304c39c265afd81e1062bf0cad34bac86632fd
Ivan Yashkov <yashkov.2021@stud.nstu.ru>
2024-05-31 04:38:15 +0700

    Финальная версия

--- 6 soft 3 all_calc/Constants.cs
+++ 6 soft 3 all_calc/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _6_soft_3_all_calc
+﻿namespace _6_soft_3_all_calc
 {
 	public static class Constants
 	{
